8c72fe50c6af6232c510da5f282062e53284f1da
kentnathaniel <kent.nathaniel2@gmail.com>
2024-05-01 11:53:04 +0700

    feat: initial commit

--- src/04-generics-advanced/20-spotting-useless-generics.problem.ts
+++ src/04-generics-advanced/20-spotting-useless-generics.problem.ts
@@ -1,20 +1,24 @@
-import { expect, it } from "vitest";
-import { Equal, Expect } from "../helpers/type-utils";
+import { expect, it } from 'vitest'
+import { Equal, Expect } from '../helpers/type-utils'
 
-const returnBothOfWhatIPassIn = <T1, T2>(params: {
-  a: T1;
-  b: T2;
-}): [T1, T2] => {
-  return [params.a, params.b];
-};
+const returnBothOfWhatIPassIn = <
+  TParams extends {
+    a: unknown
+    b: unknown
+  }
+>(
+  params: TParams
+): [TParams['a'], TParams['b']] => {
+  return [params.a, params.b]
+}
 
-it("Should return a tuple of the properties a and b", () => {
+it('Should return a tuple of the properties a and b', () => {
   const result = returnBothOfWhatIPassIn({
-    a: "a",
+    a: 'a',
     b: 1,
-  });
+  })
 
-  expect(result).toEqual(["a", 1]);
+  expect(result).toEqual(['a', 1])
 
-  type test1 = Expect<Equal<typeof result, [string, number]>>;
-});
+  type test1 = Expect<Equal<typeof result, [string, number]>>
+})
